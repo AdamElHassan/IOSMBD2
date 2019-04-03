1d3c934311d32b2c71ed7f034c70e8060475abd9
Adam el Hassan <adamelhassan@Adams-Mac.local>
2019-04-03 14:40:45 -0700

    added all the views and files needed

---
+++ IOSMBD2App/IOSMBD2App/ViewControllers/GameDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GameDetailViewController.swift
+//  IOSMBD2App
+//
+//  Created by Adam el Hassan on 03/04/2019.
+//  Copyright © 2019 Adam el Hassan. All rights reserved.
+//
+
+import Foundation
---
+++ IOSMBD2App/IOSMBD2App/ViewControllers/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  IOSMBD2App
+//
+//  Created by Adam el Hassan on 03/04/2019.
+//  Copyright © 2019 Adam el Hassan. All rights reserved.
+//
+
+import Foundation
